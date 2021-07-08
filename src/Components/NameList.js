763ba40615b9cc1991643f467064d60e1a795db8
NeginDn <negin.derakhshaan@gmail.com>
2021-07-08 15:29:18 +0100

    Funcionalities and style Added

---
+++ src/Components/NameList.js
@@ -0,0 +1,25 @@
+import "./NameList.css";
+
+const NameList = ({ names, handleClick }) => {
+
+  return (
+    <ul className="name-list-container">
+      {names.map((nameObj) => (
+        <li
+          onClick={() => handleClick(nameObj)}
+          className={
+            nameObj.name === "Oksana" || nameObj.name === "Negin"
+              ? "our-name name-card"
+              : nameObj.sex === "f"
+              ? "girl-name name-card"
+              : "boy-name name-card"
+          }
+        >
+          {nameObj.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NameList;
